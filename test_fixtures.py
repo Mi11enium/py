10d4830e9630857d1e377b4b34e9d4a679da913b
Anatoly Nikolaev <67075366+Mi11enium@users.noreply.github.com>
2026-05-13 21:35:16 +0300

    pytest-first

---
+++ test_fixtures.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def browser():
+    print("Browser")
+    yield
+    print("Browser closed")
+
+@pytest.fixture
+def login_page(browser):
+    print("Login page")
+    pass
+
+@pytest.fixture
+def user():
+    print("User!")
+    return "username", "password"
+
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
+
+def test_logout(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
+
+
